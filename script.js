375491323937f234e721ad80c81cc2eb27f9a140
Mark Oleo <oleo.mark@gmail.com>
2021-03-23 09:59:02 -0700

    Finishing core html + half of the JS functions needed

--- script.js
+++ script.js
@@ -1,38 +1,96 @@
-/*----- constants -----*/
 // - PLAYER & WINNER
-const currentPlayer = 1;
-const winner = 0;
+let currentPlayer = 1;
+let winner = 0;
 // - Color value for each player
-// - Winning combination of the array
+const colors = {
+    '1': "red",
+    '-1': "blue"
+};
+// - Winning combination 
+const winningCondition = [
+    [0, 1, 2, 3],
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [6, 7, 8, 9],
+    [10, 11, 12, 13],
+    [11, 12, 13, 14],
+    [15, 16, 17, 18],
+    [16, 17, 18, 19],
+    [0, 5, 10, 15],
+    [1, 6, 11, 16],
+    [2, 7, 12, 17],
+    [3, 8, 13, 18],
+    [4, 9, 14, 19],
+    [0, 6, 12, 18],
+    [1, 7, 13, 19],
+    [4, 8, 12, 16],
+    [3, 7, 11, 15],
+];
 
 /*----- app's state (variables) -----*/
 // - The Board's slots (The 20 cells)
+const cells = document.querySelectorAll('#board td' )
 // - Display header for whos TURN is it
 // - Display header/text for a winner or tie
 // - The winner
+let board, turn;
 
 
 /*----- cached element references -----*/
 // HTML elements that will be manipulated
 const p1ScoreEl = document.querySelector('#player-1-score');
 const p2ScoreEl = document.querySelector('#player-2-score');
+const mainHeaderEl = document.querySelector('#display-header');
 //Header text to change between players and game results
 
 /*----- event listeners -----*/
 //      button to start the game
+document.querySelector('button').addEventListener('click', gameStart);
 //      click grid/slot selection 
+const cellClicked = document.querySelector("#board").cellClicked.addEventListener("click");
+
 
 /*----- functions -----*/
 //Initialize function to start the game (which will display player 1's turn)
+function initialize() {
+    scores = {
+        p1: 0,
+        p2: 0,
+    };
+    cells = null;
+    turn = 1;
+    winner = null;
+    render();
+}
+
+function gameStart()
 //Function to display whos turn is it by text (Player 1's Turn)
+   // initiate a null variable, during game intiate change the p1 
 //Function to fill grid slot with color by players choice
+function changeCellColor() // or class?
 //Function to change player after input
+function switchPlayer(currentPlayer) {
+    if (currentPlayer === 'p1') {
+        player = 'p2';
+    } else {
+        player = 'p1';
+    }
+}
 //Function to check if that grid slot is taken, return null
+function checkCell() 
 //          Repeat Above 3
 //Function to check winning combinations
+function checkWinCondition() 
 //Function to display results if win/tie/fullboard
 //Function to reset game/clear board
+function restartGame() 
 
+function render() {
+    p1ScoreEl.textContent = scores.p1;
+    p2ScoreEl.textContent = scores.p2;
+}
+
+initialize();
 
 
 // 1) Upon loading the game it should:
@@ -57,3 +115,8 @@ const p2ScoreEl = document.querySelector('#player-2-score');
 //                          - then display tie/play again
 // 4) Handle a player clicking the start game button again
 //                   - game resets
+
+// ICEBOX STUFF WOULD BE
+// - Different grid sizes
+// - Playing vs the CP
+// 
