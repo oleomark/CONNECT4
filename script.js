a0cc703ac50f3464bf3e0b29166f7147babbe88f
Mark Oleo <oleo.mark@gmail.com>
2021-03-24 09:18:20 -0700

    50% Progress with functions

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // - PLAYER & WINNER
-let currentPlayer = 1;
-let winner = 0;
-// - Color value for each player
-const colors = {
-    '1': "red",
-    '-1': "blue"
-};
+let profiles = {
+    p1: ['1', "red"],
+    p2: ['-1', "blue"],
+    winner: ['0', "white"]
+}
+const displayMsgs = ["", "Player 1's Turn", "Player 2's Turn", "Player 1 WINS!", "Player 2 WINS!", "DRAW!"]
+let board
+let PlayerOrder = 0;
 // - Winning combination 
-const winningCondition = [
+const winningLines = [
     [0, 1, 2, 3],
     [1, 2, 3, 4],
     [5, 6, 7, 8],
@@ -25,69 +26,117 @@ const winningCondition = [
     [1, 7, 13, 19],
     [4, 8, 12, 16],
     [3, 7, 11, 15],
-];
+]
 
 /*----- app's state (variables) -----*/
 // - The Board's slots (The 20 cells)
-const cells = document.querySelectorAll('#board td' )
-// - Display header for whos TURN is it
-// - Display header/text for a winner or tie
-// - The winner
-let board, turn;
+const cells = document.querySelectorAll('#board td');
 
 
 /*----- cached element references -----*/
 // HTML elements that will be manipulated
 const p1ScoreEl = document.querySelector('#player-1-score');
 const p2ScoreEl = document.querySelector('#player-2-score');
-const mainHeaderEl = document.querySelector('#display-header');
-//Header text to change between players and game results
+const displayMsg = document.querySelector('#display-header');
+// - Display header for TURN & RESULTS
 
 /*----- event listeners -----*/
 //      button to start the game
-document.querySelector('button').addEventListener('click', gameStart);
-//      click grid/slot selection 
-const cellClicked = document.querySelector("#board").cellClicked.addEventListener("click");
+document.querySelector('button').addEventListener('click', initialize);
+// listener for every cell
+// for (let i = 0; i < td.length; i++) {
+//     td[i].addEventListener('click', placeToken)
+//     td[i].addEventListener('mouseover', checkBoard)
+// // }
+cells.addEventListener('click', playerTurn);
+cells.addEventListener('mouseover', checkBoard);
+// const cellTaken = document.querySelector("#board td", placeToken);
+
+
 
 
 /*----- functions -----*/
-//Initialize function to start the game (which will display player 1's turn)
+//Initialize data to start the game
 function initialize() {
     scores = {
-        p1: 0,
-        p2: 0,
+        p1ScoreEl: 0,
+        p2ScoreEl: 0,
     };
+    displayMsg.innerHTML = displayMsgs[1];
     cells = null;
-    turn = 1;
+    PlayerOrder = 1;
     winner = null;
     render();
 }
 
-function gameStart()
-//Function to display whos turn is it by text (Player 1's Turn)
-   // initiate a null variable, during game intiate change the p1 
-//Function to fill grid slot with color by players choice
-function changeCellColor() // or class?
-//Function to change player after input
-function switchPlayer(currentPlayer) {
-    if (currentPlayer === 'p1') {
-        player = 'p2';
-    } else {
-        player = 'p1';
+//Function to change player 
+function playerOrder() {
+    if (playerOrder === 1) {
+        displayMsg.innerHTML = displayMsgs[1];
+    }else{
+        displayMsg.innerHTML = displayMsgs[2];
     }
 }
+function playerTurn(cell) {
+    let idx = cells.target.id;
+         if (cells.classList.contains('RedToken' || 'BlueToken') {
+            return; 
+    }
+        if (playerOrder === 1 && winner === 0) {
+                cells.target.childNodes[0].style.backgroundColor = profiles['p1'][1];
+                cells.classList.add('RedToken')
+        }else if (playerOrder === -1 && winner === 0) {
+                cells.target.childNodes[0].style.backgroundColor = profiles['p2'][1];
+                cells.classList.add('BlueToken')
+        }
+        turn *= -1;
+        winner = checkBoard();
+        render();
+}
+
+// function playerMove(cell) { 
+//     // console.log(cell)
+//     // console.log(cell.target.childNodes)
+//     // console.log(idx)
+//     // if (turn === 1) {
+//         // }else{
+//             //     cellChange = cell.target.childNodes[0].style.backgroundColor = "blue";
+//             // }
+//             // turn *= -1;
+//             // winner = getWinner();
+//             // render()
+//         }
+
+//Function to fill grid slot with color by players choice
+// CHECKING ROW,COLUMNS, IF NOT FILLED, THEN FILL , IF FILLEd PUSH ONTO ROW 3,
 //Function to check if that grid slot is taken, return null
-function checkCell() 
-//          Repeat Above 3
-//Function to check winning combinations
-function checkWinCondition() 
-//Function to display results if win/tie/fullboard
-//Function to reset game/clear board
-function restartGame() 
+// 
+
+// function checkWinCondition() 
+// Taking the 4 values of the array and compare
+function checkBoard() {
+    for(let i = 0; i < winningLines.length; i++) {
+        const i1 = td[winningLines[i][0]];
+        const i2 = td[winningLines[i][1]];
+        const i3 = td[winningLines[i][2]];
+        const i4 = td[winningLines[i][3]];
+}
+// //Function to check winning combinations
+
+// function restartGame() 
+// //Function to reset game/clear board
 
 function render() {
     p1ScoreEl.textContent = scores.p1;
     p2ScoreEl.textContent = scores.p2;
+    if (winner === "1") {
+        mainHeaderEl.textContent = displayMsgs[3];
+    }else if (winner === "-1") {
+        mainHeaderEl.textContent = displayMsgs[4];
+    }else if (winner === "DRAW") {
+        mainHeaderEl.textContent = displayMsgs[5];
+    }
+    scores[winner]++;
 }
 
 initialize();
